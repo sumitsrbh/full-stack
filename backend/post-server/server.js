a369d0e52b306a8e59265c14d3b34869024c2c8e
sumit sourabh <sumitsrbh@gmail.com>
2026-08-19 06:25:21 +0530

    new codes- cookbook

---
+++ backend/post-server/server.js
@@ -0,0 +1,47 @@
+const http = require('node:http')
+const fs = require('node:fs')
+const path = require('node:path')
+
+const form = fs.readFileSync(path.join(__dirname, 'public', 'form.html'))
+
+http
+  .createServer((req, res) => {
+    if (req.method === 'GET') {
+      get(res)
+      return
+    }
+    if (req.method === 'POST') {
+      post(req, res)
+      return
+    }
+    error(405, res)
+  })
+  .listen(3001, () => console.log('Server running on http://localhost:3001/'))
+
+function get(res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+  })
+  res.end(form)
+}
+
+function post(req, res) {
+  if (req.headers['content-type'] !== 'application/x-www-form-urlencoded') {
+    error(415, res)
+    return
+  }
+
+  let input = ''
+  req.on('data', (chunk) => {
+    input += chunk.toString()
+  })
+  req.on('end', () => {
+    console.log(input)
+    res.end(http.STATUS_CODES[200])
+  })
+}
+
+function error(code, res) {
+  res.statusCode = code
+  res.end(http.STATUS_CODES[code])
+}
