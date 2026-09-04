3cf4440581689e8d94371fd13cf9a0562f15e60d
sumit sourabh <sumitsrbh@gmail.com>
2026-09-04 21:01:30 +0530

    implemented http GET

--- full-stack-app/real-time-taskboard/server/router.js
+++ full-stack-app/real-time-taskboard/server/router.js
@@ -8,11 +8,33 @@ const router = {
       method: 'GET',
     })
   },
+
   handle: (req, res) => {
-    const route = routes.find(
-      (route) => route.path === req.url && route.method === 'GET',
-    )
+    let route = null
+    req.params = {}
+    const urlArry = req.url.split('/').filter(Boolean)
+    route = routes.find((r) => {
+      let equal = true
+      const pathArry = r.path.split('/').filter(Boolean)
+      if (pathArry.length === urlArry.length && equal) {
+        for (let i = 0; i < urlArry.length; i++) {
+          if (urlArry[i] == pathArry[i]) {
+            equal = true
+          } else if (pathArry[i].startsWith(':')) {
+            let prop = pathArry[i].replace(':', '')
+            req.params[prop] = urlArry[i]
+            console.log('params', req.params)
+          } else {
+            equal = false
+          }
+        }
+      } else {
+        equal = false
+      }
+      return equal ? r : null
+    })
     if (route) {
+      console.log('route:', route, 'req.params:', req.params)
       route.handler(req, res)
     } else {
       res.statusCode = 404
